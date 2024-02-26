97332712d8e949543bade63951a40f8d35a2c51f
hirokazuk <hirokazukamiya@gmail.com>
2024-02-26 14:46:00 +0900

    remove configure

--- Formula/myshellutils.rb
+++ Formula/myshellutils.rb
@@ -15,7 +15,7 @@ class Myshellutils < Formula
   def install
     # Remove unrecognized options if they cause configure to fail
     # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
-    system "./configure", "--disable-silent-rules", *std_configure_args
+    # system "./configure", "--disable-silent-rules", *std_configure_args
     # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
     bin.install "bin/testecho.sh"
     #prefix.install 'bin'

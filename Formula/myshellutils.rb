386a5fcf11a7a2e1b4094ddaa38d1e06efccc9e1
hirokazuk <hirokazukamiya@gmail.com>
2024-02-26 14:42:31 +0900

    fix bin.install

--- Formula/myshellutils.rb
+++ Formula/myshellutils.rb
@@ -17,8 +17,9 @@ class Myshellutils < Formula
     # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
     system "./configure", "--disable-silent-rules", *std_configure_args
     # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
-    prefix.install 'bin'
-    (bin+'testecho.sh').chmod 0755
+    bin.install "bin/testecho.sh"
+    #prefix.install 'bin'
+    #(bin+'testecho.sh').chmod 0755
   end
 
   test do

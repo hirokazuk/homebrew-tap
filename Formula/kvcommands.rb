35f73cdee6fc91122203a358e0d33b68add78296
hirokazuk <hirokazukamiya@gmail.com>
2024-02-27 15:51:10 +0900

    rename

--- Formula/kvcommands.rb
+++ Formula/kvcommands.rb
@@ -1,7 +1,7 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-class KVCommands < Formula
+class Kvcommands < Formula
   desc "kvcommands"
   homepage ""
   url "https://github.com/hirokazuk/kv_commands/archive/refs/tags/v0.0.1.tar.gz"

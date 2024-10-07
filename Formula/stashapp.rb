53d8fa9f32a69440fc07809f86738ca7eef19350
hirokazuk <hirokazukamiya@gmail.com>
2024-10-07 17:08:24 +0900

    fix

--- Formula/stashapp.rb
+++ Formula/stashapp.rb
@@ -1,33 +1,28 @@
+
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 OWNER="stashapp"
 REPO="stash"
 VERSION="0.26.2"
-
 class Stashapp < Formula
   desc "#{OWNER}/#{REPO}"
   homepage "https://github.com/#{OWNER}/#{REPO}"
   url "https://github.com/#{OWNER}/#{REPO}/releases/download/v#{VERSION}/stash-macos"
+  version VERSION
   #sha256 ""
   license "private"
-
-  option "with-target-version", "Specify version to install"
+  #head "https://github.com/#{OWNER}/#{REPO}.git", branch: "main"
 
   head do
     url "https://github.com/stashapp/stash/releases/download/latest_develop/stash-macos"
   end
 
   def install
-    if build.with? "target-version"
-      version = ARGV.value("with-target-version")
-      url = "https://github.com/#{OWNER}/#{REPO}/releases/download/v#{version}/stash-macos"
-      curl_download url, to: "stash-macos"
-    end
-    
     bin.install "stash-macos" => "stash"
   end
 
   test do
-    system "stash", "--version"
+    system "stash" , "--version"
   end
 end

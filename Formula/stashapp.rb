5f39f66e583330496955319115c08423f8ebe405
hirokazuk <hirokazukamiya@gmail.com>
2025-11-18 21:06:13 +0900

    Update Stashapp formula description to reflect version change from 2 to 3

--- Formula/stashapp.rb
+++ Formula/stashapp.rb
@@ -6,7 +6,7 @@ OWNER="stashapp"
 REPO="stash"
 VERSION=ENV["STASHAPP_DEVEL_VERSION"] || "0.29.3"
 class Stashapp < Formula
-  desc "#{OWNER}/#{REPO} @2"
+  desc "#{OWNER}/#{REPO} @3"
   homepage "https://github.com/#{OWNER}/#{REPO}"
   # puts "HOMEBREW_STASHAPP_VERSION: #{ENV["HOMEBREW_STASHAPP_VERSION"]}"
   # if ENV["HOMEBREW_STASHAPP_VERSION"]
